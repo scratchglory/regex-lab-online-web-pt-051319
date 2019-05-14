358b4c0e5227c2155a02568af783339f0fdca2e6
Adella <adellakenn@gmail.com>
2019-05-14 18:34:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
   word.match?(/[aeiou]/)
-  word.match?(/^[bcdfghjklmnpqrstvwxyz]/)
+  # word.match?(/^[bcdfghjklmnpqrstvwxyz]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
